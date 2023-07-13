2c089dde8353099402f56ed1f110b46fca6f1163
Sofia <misofser@gmail.com>
2023-07-13 08:16:15 +0300

    Add projects pages functionality and route

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,9 @@
+class ProjectsController < ApplicationController
+
+  def project_a
+  end
+  
+  def project_b
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
 
   get '/contacts', to: 'pages#contacts', as: 'contacts'
 
+  get '/projects/project_a', to: 'projects#project_a', as: 'project_a'
+  get '/projects/project_b', to: 'projects#project_b', as: 'project_b'
+
 end
